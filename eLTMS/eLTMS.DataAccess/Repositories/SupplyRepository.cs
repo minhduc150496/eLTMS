1550ba905484daa3ef3348271f3abd5c65a79e2c
dung tran <ttdung410@gmail.com>
2018-09-30 17:14:31 +0700

    aaa

---
+++ eLTMS/eLTMS.DataAccess/Repositories/SupplyRepository.cs
@@ -0,0 +1,28 @@
+﻿using eLTMS.DataAccess.Infrastructure;
+using eLTMS.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+
+namespace eLTMS.DataAccess.Repositories
+{
+    
+    public interface ISupplyRepository : IRepository<Supply>
+    {
+        List<Supply> GetAllSupply();
+    }
+    public class SupplyRepository : RepositoryBase<Supply>, ISupplyRepository
+    {
+        public List<Supply> GetAllSupply()
+        {
+            var result = DbSet.AsQueryable()
+                .Include( x => x.SupplyType)
+                .ToList();
+            return result;
+        }
+    }
+}
